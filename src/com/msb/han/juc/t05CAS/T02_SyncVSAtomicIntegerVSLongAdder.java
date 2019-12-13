ba75ce101cfab56b7a072745e5952ae4641babd6
shaopeng007 <h18947157403@yeah.net>
2019-12-13 23:58:41 +0800

    新增

--- src/com/msb/han/juc/t05CAS/T02_SyncVSAtomicIntegerVSLongAdder.java
+++ src/com/msb/han/juc/t05CAS/T02_SyncVSAtomicIntegerVSLongAdder.java
@@ -1,16 +1,88 @@
 package com.msb.han.juc.t05CAS;
 
+import com.msb.han.juc.t01ThreadConcept.ThreadState;
+
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.LongAdder;
 
 public class T02_SyncVSAtomicIntegerVSLongAdder {
 
-    Long Lcount=0L;
+    static Long Lcount=0L;
 
-    AtomicInteger Acount =new AtomicInteger(0);
+    static AtomicInteger Acount =new AtomicInteger(0);
 
-    LongAdder LAcount=new LongAdder();
+    static LongAdder LAcount=new LongAdder();
 
-    
+    public static void main(String[] args) throws InterruptedException {
+        Thread [] threads=new Thread[1000];
+
+        for (int i = 0; i <threads.length ; i++) {
+            threads[i]=new Thread(()->{
+                for (int j = 0; j <100000 ; j++) Acount.incrementAndGet();
+            });
+
+        }
+
+        long start=System.currentTimeMillis();
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        for(Thread t:threads) {
+            t.join();
+        }
+       long end=System.currentTimeMillis();
+
+        System.out.println("AtomicInteger:"+Acount.get()+"time"+(end-start));
+
+        Object lock=new Object();
+        for (int i = 0; i <threads.length ; i++) {
+            threads[i]=new Thread(()->{
+                for (int j = 0; j <100000 ; j++) {
+                    synchronized (lock){
+                        Lcount++;
+                    }
+                }
+            });
+
+
+        }
+
+        start=System.currentTimeMillis();
+
+        for (Thread t:threads) {
+            t.start();
+        }
+
+        for (Thread t:threads)t.join();
+
+        end=System.currentTimeMillis();
+
+        System.out.println("Long"+Lcount+"time"+(end-start));
+
+        for (int i = 0; i <threads.length ; i++) {
+
+            threads[i]=new Thread(()->{
+                for (int j = 0; j <100000 ; j++) {
+                    LAcount.increment();
+                }
+
+            });
+
+        }
+
+        start=System.currentTimeMillis();
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        for(Thread t:threads) {
+            t.join();
+        }
+        end=System.currentTimeMillis();
+
+        System.out.println("LongAdder:"+LAcount.longValue()+"time"+(end-start));
+    }
 
 }
